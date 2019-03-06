50c5e03922ef1cf3adfac4e2fb2953ecc940810b
yaming <1124014326@qq.com>
2019-03-06 11:36:30 +0800

    dd

--- src/router/index.js
+++ src/router/index.js
@@ -14,7 +14,7 @@ import editAddress from '@/components/address/editAddress'
 Vue.use(Router)
 
 export default new Router({
-	mode: 'history',
+	mode: 'hash',
   scrollBehavior (to, from, savePosition) {
     if (savePosition) {
       return savePosition
