706d2aeb6a3d00bb8ff69a8bfa7b016f8089e92f
yaming <1124014326@qq.com>
2019-03-06 10:16:04 +0800

    build

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -19,8 +19,8 @@ module.exports = {
     path: config.build.assetsRoot,
     filename: '[name].js',
     publicPath: process.env.NODE_ENV === 'production'
-      ? config.build.assetsPublicPath
-      : config.dev.assetsPublicPath
+      ? './' + config.build.assetsPublicPath
+      : './' + config.dev.assetsPublicPath
   },
   resolve: {
     extensions: ['.js', '.vue', '.json'],
--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -25,7 +25,8 @@ const webpackConfig = merge(baseWebpackConfig, {
   output: {
     path: config.build.assetsRoot,
     filename: utils.assetsPath('js/[name].[chunkhash].js'),
-    chunkFilename: utils.assetsPath('js/[id].[chunkhash].js')
+    chunkFilename: utils.assetsPath('js/[id].[chunkhash].js'),
+    publicPath:'./'
   },
   plugins: [
     // http://vuejs.github.io/vue-loader/en/workflow/production.html
